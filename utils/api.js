2f2904994ec8125ff7b2f9ba7e97a5c750b5e514
lucas lopes <lucaslopzz8@gmail.com>
2025-06-28 00:07:26 -0300

    Delete utils directory

--- utils/api.js
+++
@@ -1,1 +0,0 @@
-// Funções de integração com Supabase e ASAAS
--- utils/supabase.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://yztccxtalewgtzqsffcx.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dGNjeHRhbGV3Z3R6cXNmZmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwNzcxODEsImV4cCI6MjA2NjY1MzE4MX0.W-v0c19NCuAuHudrHgO29innjgh-OPKh_Ie0uhqJPtA';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);

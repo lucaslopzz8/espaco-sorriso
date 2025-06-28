be383b0c8e3c66cbfd45cfe04cc3c9d3cf56cb91
lucas lopes <lucaslopzz8@gmail.com>
2025-06-28 01:05:25 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Bem-vindo ao sistema Espaço Sorriso</h1>
-      <p>Login, Agenda, Financeiro e mais...</p>
-    </div>
-  );
-}

82cdbd07b4d43c81fc5af7a301f7aef63770ca2b
lucas lopes <lucaslopzz8@gmail.com>
2025-06-28 00:30:18 -0300

    Add files via upload

---
+++ components/MenuLateral.js
@@ -0,0 +1,1 @@
+// Menu lateral com abas
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Bem-vindo ao sistema Espaço Sorriso</h1>
+      <p>Login, Agenda, Financeiro e mais...</p>
+    </div>
+  );
+}
